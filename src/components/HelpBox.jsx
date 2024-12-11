cabf845269e04754efc81838a9b943c683e16fc4
srinath1 <srinath.erp@gmail.com>
2024-12-11 19:18:32 +0100

    checked cached

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,10 +1,11 @@
-import { string } from 'prop-types';
+import { string } from "prop-types";
 
-import './HelpBox.css';
+import "./HelpBox.css";
 
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
+      <h1>cached</h1>
       <h2>{title}</h2>
       <p>{text}</p>
     </article>
